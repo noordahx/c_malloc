eede64fc792f040c88b96bede088d8738b3bbcdc
Nurdaulet (Nurda) Taumergenov <ntaumergenov@polymercapital.com>
2024-03-25 15:48:34 +0800

    check for leaks (realloc segfaults)

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 int main() {
     printf("Hello, World!\n");
-    int *a = malloc(sizeof(int) * 2);
+    size_t *a = malloc(sizeof(size_t) * 2);
     a[0] = 1;
     a[1] = 2;
-    printf("%d %d\n", a[0], a[1]);
+    printf("%ld %ld\n", a[0], a[1]);
     free(a);
-    printf("%d %d\n", a[0], a[1]);
+
     return 0;
 }
